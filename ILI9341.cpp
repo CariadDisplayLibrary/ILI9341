043af8e2879fd4141c2336f941be7bfb50897882
Matt Jenkins <matt@majenko.co.uk>
2017-08-19 16:54:13 +0100

    Fixed warnings

--- ILI9341.cpp
+++ ILI9341.cpp
@@ -229,7 +229,7 @@ void ILI9341::windowData(color_t c) {
 }
 
 void ILI9341::windowData(color_t *c, int len) {
-    for (uint32_t i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
         data(c[i] >> 8);
         data(c[i] & 0xFF);
     }
